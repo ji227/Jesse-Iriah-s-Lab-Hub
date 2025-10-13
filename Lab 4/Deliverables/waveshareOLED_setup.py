175a5bcbe91e54c933694704fd23907a8497fa35
ji227 <ji227@cornell.edu>
2025-10-12 23:35:41 -0400

    Update waveshareOLED_setup.py

--- Lab 4/Deliverables/waveshareOLED_setup.py
+++ Lab 4/Deliverables/waveshareOLED_setup.py
@@ -2,6 +2,7 @@
 Simple Raspberry Pi OLED display script for Waveshare 2.23" OLED HAT (128x32 pixels).
 
 Key info for development:
+- Screen dimensions: 50 mm (width) × 12.5 mm (height) based on pixel pitch 0.41 mm × 0.39 mm and resolution 128×32 pixels.
 - Display resolution: 128 (width) × 32 (height) pixels.
 - Picel size: Approximately 0.41mm × 0.39mm per pixel
 - Interface: SPI by default
